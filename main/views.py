fa27d5db9ba56b94aa830cc08a044b7fb6d0f450
Christian <buoc_12@hotmail.com>
2016-04-01 19:33:16 -0600

    Juegos en home mostrandose

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from juego.models import Juego
 
-# Create your views here.
 
 class HomeView(TemplateView):
-	def get(self,request):
-		template_name="index.html"
+    def get(self, request):
+        template_name = "index.html"
+        return render(request, template_name)
 
-		return render(request, template_name)
 
+class RecientesView(TemplateView):
+    def get(self, request):
+        template = 'index.html'
+        juegos = Juego.objects.order_by('fecha_alta').reverse()
+        ctx = {'jgo': juegos}
+        return render(request, template, ctx)
