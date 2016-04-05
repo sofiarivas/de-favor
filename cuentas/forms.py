4a3129b61db256595ba50de461a9c7c78e8bfeb3
Christian <buoc_12@hotmail.com>
2016-04-04 19:24:27 -0500

    login, registro y logout -logout no redirecciona bien

---
+++ cuentas/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    email = forms.CharField(label='Correo Electronico')
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    username = forms.EmailField(label='Correo')
+    first_name = forms.CharField(label='Nombre')
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repite la contraseña',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Contraseñas no coinciden')
+        return cd['password2']
